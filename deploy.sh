a8b9a428f5b6f0d518457f9c15751af6a9f3d54e
weiguozhen <1272235678@qq.com>
2020-05-09 23:32:26 +0800

    file

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ sq2="source /root/workspace/ContinuousIntegration/db_shopping.sql"
 $mysql -e "$sq1"
 $mysql -e "$sq2"
 #关闭tomcat服务
-shutdown.sh
+sh shutdown.sh
 #开启tomcat服务
-startup.sh
+sh startup.sh
