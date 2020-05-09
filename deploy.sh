f3228d03547fa0069147261087799457bc522ced
weiguozhen <1272235678@qq.com>
2020-05-10 02:11:04 +0800

    x

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,10 @@ sq2="source /root/workspace/ContinuousIntegration/db_shopping.sql"
 $mysql -e "$sq1"
 $mysql -e "$sq2"
 #关闭tomcat服务
-/opt/apache-tomcat-7.0.77/bin/shutdown.sh
+#/opt/apache-tomcat-7.0.77/bin/shutdown.sh
 #开启tomcat服务
 #export BUILD_ID=dontkillme
 #export JENKINS_NODE_COOKIE=dontkillme
-BUILD_ID=DONTKILLME
-/opt/apache-tomcat-7.0.77/bin/startup.sh
+#BUILD_ID=DONTKILLME
+#/opt/apache-tomcat-7.0.77/bin/startup.sh
+sh 'JENKINS_NODE_COOKIE=dontKillMe nohup /opt/apache-tomcat-7.0.77/bin/startup.sh &'
