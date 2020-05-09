3c0309c8d89d89e5119e6777c85b133fcba7c167
weiguozhen <1272235678@qq.com>
2020-05-10 02:20:39 +0800

    x

--- deploy.sh
+++ deploy.sh
@@ -15,4 +15,4 @@ $mysql -e "$sq2"
 #export JENKINS_NODE_COOKIE=dontkillme
 #BUILD_ID=DONTKILLME
 #/opt/apache-tomcat-7.0.77/bin/startup.sh
-sh 'nohup /opt/apache-tomcat-7.0.77/bin/startup.sh &'
+/opt/apache-tomcat-7.0.77/bin/startup.sh
