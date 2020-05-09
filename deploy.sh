fa2afe35d38ed74cdfa640693517b05ff5d8698c
weiguozhen <1272235678@qq.com>
2020-05-10 01:48:13 +0800

    x

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #将项目拷贝到tomcat目录下
-export BUILD_ID=dontkillme
-export JENKINS_NODE_COOKIE=dontKillMe
+
 cp -r /root/workspace/ContinuousIntegration/Shopping /opt/apache-tomcat-7.0.77/webapps
 #登陆数据库删除库导入库
 mysql="mysql --defaults-extra-file=/root/.my.cnf"
@@ -12,5 +11,7 @@ $mysql -e "$sq2"
 #关闭tomcat服务
 sh /opt/apache-tomcat-7.0.77/bin/shutdown.sh
 #开启tomcat服务
+export BUILD_ID=dontkillme
+export JENKINS_NODE_COOKIE=dontKillMe
 sh /opt/apache-tomcat-7.0.77/bin/startup.sh
 sleep 60
