a5b51ea53f19ede05fbcceb25a49e4f5cc717930
weiguozhen <1272235678@qq.com>
2020-05-11 00:53:05 +0800

    haha

--- deploy.sh
+++ deploy.sh
@@ -13,13 +13,13 @@ else
     echo "The tomcat process has been killed"
 fi
     #将项目拷贝到tomcat目录下
-    cp -r /root/workspace/ContinuousIntegration/Shopping /home/wgz/usr/apache-tomcat-7.0.77/webapps
+    cp -r /root/workspace/ContinuousIntegration/Shopping /home/wgz/usr/apache-tomcat-7/webapps
     #登陆数据库删除库导入库, 针对我当前环境，实际情况不要这么做
     mysql="mysql --defaults-extra-file=/root/.my.cnf"
     sq1="drop database if exists db_shopping"
     sq2="source /root/workspace/ContinuousIntegration/db_shopping.sql"
     $mysql -e "$sq1"
     $mysql -e "$sq2"
-    export EXEC=/opt/apache-tomcat-7.0.77/bin/startup.sh
+    export EXEC=/opt/apache-tomcat-7/bin/startup.sh
     nohup $EXEC start >& /tmp/run.log &
 #BUILD_ID=dontKillMe
