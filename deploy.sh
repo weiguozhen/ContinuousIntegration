a420522305081f55b9761a9fe26495fbe767c5a4
weiguozhen <1272235678@qq.com>
2020-05-11 00:50:27 +0800

    haha

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 export BUILD_ID=172.20.10.5 #必须加
-TOMCAT_PID=`ps aux | grep 'tomcat' | grep -v grep | awk '{print $2}'`
+#TOMCAT_PID=`ps aux | grep 'tomcat' | grep -v grep | awk '{print $2}'`
+TOMCATTHREAD=`ps aux | grep 'tomcat' | grep -v grep`
 if test -z "$TOMCATTHREAD"
 then
    echo "TOMCAT NOT START"
 else
-    kill -9 $TOMCAT_PID
+    THREADLIST=(${TOMCATTHREAD// / })
+    PID=${THREADLIST[1]}
+    kill -9 $PID
     sleep 6s
     echo "The tomcat process has been killed"
 fi
