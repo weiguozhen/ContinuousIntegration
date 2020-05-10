fca859a2050e64d7ffd18b8df3799c77a61e1f4c
weiguozhen <1272235678@qq.com>
2020-05-11 00:39:11 +0800

    haha

--- deploy.sh
+++ deploy.sh
@@ -10,15 +10,3 @@ else
     kill -9 $TOMCAT_PID
     sleep 6s
     echo 'The tomcat process has been killed'
-#fi
-#    #将项目拷贝到tomcat目录下
-#    cp -r /root/workspace/ContinuousIntegration/Shopping /opt/apache-tomcat-7.0.77/webapps
-#    #登陆数据库删除库导入库, 针对我当前环境，实际情况不要这么做
-#    mysql="mysql --defaults-extra-file=/root/.my.cnf"
-#    sq1="drop database if exists db_shopping"
-#    sq2="source /root/workspace/ContinuousIntegration/db_shopping.sql"
-#    $mysql -e "$sq1"
-#    $mysql -e "$sq2"
-#    export EXEC=/opt/apache-tomcat-7.0.77/bin/startup.sh
-#    nohup $EXEC start >& tmp/run.log &
-##BUILD_ID=dontKillMe
